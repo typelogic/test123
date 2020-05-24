a354dc86531140a28bd5d3031a1650cfc1773302
dexter <dexter@newlogic.io>
2020-05-24 23:44:37 +0800

    init

---
+++ src/main/java/smeag/Smeagol.java
@@ -0,0 +1,15 @@
+package smeag;
+
+/**
+ * Hello world!
+ */
+public class Smeagol {
+    public int addnum(int a, int b) {
+        return a + b;
+    }
+
+    public int subnum(int a, int b) {
+        return a - b;
+    }
+
+}
